78969d8b7c02c10623ec917a862ee2e33dd99efd
shexingju <1970237065@qq.com>
2024-06-16 00:54:19 +0800

    发送串口产生段错误

--- real_time_stats/main/real_time_stats_example_main.c
+++ real_time_stats/main/real_time_stats_example_main.c
@@ -15,155 +15,48 @@
 #include "freertos/semphr.h"
 #include "esp_err.h"
 #include "station_wifi.h"
+#include "bus_serial_driver.h"
 
 static const char *TAG = "task simple";
 
-#define NUM_OF_SPIN_TASKS   6
-#define SPIN_ITER           500000  //Actual CPU cycles used will depend on compiler optimization
-#define SPIN_TASK_PRIO      2
-#define STATS_TASK_PRIO     3
-#define STATS_TICKS         pdMS_TO_TICKS(1000)
-#define ARRAY_SIZE_OFFSET   5   //Increase this if print_real_time_stats returns ESP_ERR_INVALID_SIZE
-
-static SemaphoreHandle_t sync_spin_task;
-static SemaphoreHandle_t sync_stats_task;
-
-/**
- * @brief   Function to print the CPU usage of tasks over a given duration.
- *
- * This function will measure and print the CPU usage of tasks over a specified
- * number of ticks (i.e. real time stats). This is implemented by simply calling
- * uxTaskGetSystemState() twice separated by a delay, then calculating the
- * differences of task run times before and after the delay.
- *
- * @note    If any tasks are added or removed during the delay, the stats of
- *          those tasks will not be printed.
- * @note    This function should be called from a high priority task to minimize
- *          inaccuracies with delays.
- * @note    When running in dual core mode, each core will correspond to 50% of
- *          the run time.
- *
- * @param   xTicksToWait    Period of stats measurement
- *
- * @return
- *  - ESP_OK                Success
- *  - ESP_ERR_NO_MEM        Insufficient memory to allocated internal arrays
- *  - ESP_ERR_INVALID_SIZE  Insufficient array size for uxTaskGetSystemState. Trying increasing ARRAY_SIZE_OFFSET
- *  - ESP_ERR_INVALID_STATE Delay duration too short
- */
-static esp_err_t print_real_time_stats(TickType_t xTicksToWait)
+static void usart_recv_task(void *arg)
 {
-    TaskStatus_t *start_array = NULL, *end_array = NULL;
-    UBaseType_t start_array_size, end_array_size;
-    uint32_t start_run_time, end_run_time;
-    esp_err_t ret;
-
-    //Allocate array to store current task states
-    start_array_size = uxTaskGetNumberOfTasks() + ARRAY_SIZE_OFFSET;
-    start_array = malloc(sizeof(TaskStatus_t) * start_array_size);
-    if (start_array == NULL) {
-        ret = ESP_ERR_NO_MEM;
-        goto exit;
+    bus_serial_driver_t* serial = (bus_serial_driver_t*)arg;
+    while (1) {
+        bus_sync_rx((bus_driver_t**)serial);
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
-    //Get current task states
-    start_array_size = uxTaskGetSystemState(start_array, start_array_size, &start_run_time);
-    if (start_array_size == 0) {
-        ret = ESP_ERR_INVALID_SIZE;
-        goto exit;
-    }
-
-    vTaskDelay(xTicksToWait);
-
-    //Allocate array to store tasks states post delay
-    end_array_size = uxTaskGetNumberOfTasks() + ARRAY_SIZE_OFFSET;
-    end_array = malloc(sizeof(TaskStatus_t) * end_array_size);
-    if (end_array == NULL) {
-        ret = ESP_ERR_NO_MEM;
-        goto exit;
-    }
-    //Get post delay task states
-    end_array_size = uxTaskGetSystemState(end_array, end_array_size, &end_run_time);
-    if (end_array_size == 0) {
-        ret = ESP_ERR_INVALID_SIZE;
-        goto exit;
-    }
-
-    //Calculate total_elapsed_time in units of run time stats clock period.
-    uint32_t total_elapsed_time = (end_run_time - start_run_time);
-    if (total_elapsed_time == 0) {
-        ret = ESP_ERR_INVALID_STATE;
-        goto exit;
-    }
-
-    printf("| Task | Run Time | Percentage\n");
-    //Match each task in start_array to those in the end_array
-    for (int i = 0; i < start_array_size; i++) {
-        int k = -1;
-        for (int j = 0; j < end_array_size; j++) {
-            if (start_array[i].xHandle == end_array[j].xHandle) {
-                k = j;
-                //Mark that task have been matched by overwriting their handles
-                start_array[i].xHandle = NULL;
-                end_array[j].xHandle = NULL;
-                break;
-            }
-        }
-        //Check if matching task found
-        if (k >= 0) {
-            uint32_t task_elapsed_time = end_array[k].ulRunTimeCounter - start_array[i].ulRunTimeCounter;
-            uint32_t percentage_time = (task_elapsed_time * 100UL) / (total_elapsed_time * portNUM_PROCESSORS);
-            printf("| %s | %"PRIu32" | %"PRIu32"%%\n", start_array[i].pcTaskName, task_elapsed_time, percentage_time);
-        }
-    }
-
-    //Print unmatched tasks
-    for (int i = 0; i < start_array_size; i++) {
-        if (start_array[i].xHandle != NULL) {
-            printf("| %s | Deleted\n", start_array[i].pcTaskName);
-        }
-    }
-    for (int i = 0; i < end_array_size; i++) {
-        if (end_array[i].xHandle != NULL) {
-            printf("| %s | Created\n", end_array[i].pcTaskName);
-        }
-    }
-    ret = ESP_OK;
-
-exit:    //Common return path
-    free(start_array);
-    free(end_array);
-    return ret;
 }
 
-static void spin_task(void *arg)
+static void usart_send_task(void *arg)
 {
-
+    bus_serial_driver_t* serial = (bus_serial_driver_t*)arg;
     while (1) {
-        printf("test task .......\n");
+        bus_sync_tx((bus_driver_t**)serial);
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
+
+static void recv_task(void *arg)
+{
+    uint8_t buffer[256];
+    bus_serial_driver_t* serial = (bus_serial_driver_t*)arg;
+    while (1) {
+        bus_read((bus_driver_t**)serial, buffer, 256);     
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
+
+static void send_task(void *arg)
+{
+    uint8_t buffer[256] = {0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08};
+    bus_serial_driver_t* serial = (bus_serial_driver_t*)arg;
+    while (1) {   
+        bus_write((bus_driver_t**)serial,buffer,10);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
-static void stats_task(void *arg)
-{
-    xSemaphoreTake(sync_stats_task, portMAX_DELAY);
-
-    //Start all the spin tasks
-    for (int i = 0; i < NUM_OF_SPIN_TASKS; i++) {
-        xSemaphoreGive(sync_spin_task);
-    }
-
-    //Print real time stats periodically
-    while (1) {
-        printf("\n\nGetting real time stats over %"PRIu32" ticks\n", STATS_TICKS);
-        if (print_real_time_stats(STATS_TICKS) == ESP_OK) {
-            printf("Real time stats obtained\n");
-        } else {
-            printf("Error getting real time stats\n");
-        }
-        vTaskDelay(pdMS_TO_TICKS(60000));
-    }
-}
 
 void app_main(void)
 {
@@ -178,13 +71,12 @@ void app_main(void)
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
 
-    //Create semaphores to synchronize
-    sync_spin_task = xSemaphoreCreateCounting(NUM_OF_SPIN_TASKS, 0);
-    sync_stats_task = xSemaphoreCreateBinary();
 
-    xTaskCreatePinnedToCore(spin_task, "task_name", 1024, NULL, SPIN_TASK_PRIO, NULL, tskNO_AFFINITY);
+    bus_serial_driver_t* serial = bus_serial_driver_register("ttyEsp", 1);
+    bus_init((bus_driver_t**)serial);
 
-    //Create and start stats task
-    xTaskCreatePinnedToCore(stats_task, "stats", 4096, NULL, STATS_TASK_PRIO, NULL, tskNO_AFFINITY);
-    xSemaphoreGive(sync_stats_task);
+    xTaskCreatePinnedToCore(usart_recv_task, "usart_recv_task", 1024, serial, 3, NULL, tskNO_AFFINITY);
+    xTaskCreatePinnedToCore(usart_send_task, "usart_send_task", 1024, serial, 3, NULL, tskNO_AFFINITY);
+    xTaskCreatePinnedToCore(recv_task, "recv_task", 1024, serial, 3, NULL, tskNO_AFFINITY);
+    xTaskCreatePinnedToCore(send_task, "send_task", 1024, serial, 3, NULL, tskNO_AFFINITY);
 }
